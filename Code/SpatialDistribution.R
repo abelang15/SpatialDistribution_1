2ab5f87dd2d8e69e2af8fc0d7db53e498a7653a4
abelang15 <abela15@lsu.edu>
2023-08-23 03:16:46 -0500

    almost there 2

--- Code/SpatialDistribution.R
+++ Code/SpatialDistribution.R
@@ -24,10 +24,10 @@ Bison.sp <- SpatialPoints(cbind(Bison$decimallongitude, Bison$decimallatitude))
 proj4string(Bison.sp) <- "+proj=longlat +datum=WGS84 +no_defs"
 
 
-sdbound <- readOGR("./NBD_Boundry/GOVTUNIT_Montana_State_Shape/Shape/GU_CountyOrEquivalent.shp", layer = "GU_CountyOrEquivalent")
+sdbound <- readOGR("C:/Users/abela/OneDrive/Desktop/SpatialDistribution_1/NBD_Boundry/COUNTY_BOUNDRY/FWS_National_Wilderness_Ext.shp", layer = "FWS_National_Wilderness_Ext")
 proj4string(sdbound)
 
-dem <- raster("elev_data.tif")
+dem <- raster("DEM")
 proj4string(dem)
 
 
@@ -35,7 +35,7 @@ setwd("Bioclim_tiles")
 bioclim <- stack(list.files(pattern = "*.tif$", full.names = TRUE))
 
 
-# Create copies of the San Diego Boundary, projected to the same projections
+# Create copies of the Boundary, projected to the same projections
 # as the DEM and Bioclim rasters, respectively
 sdbound.dem <- spTransform(sdbound, crs(dem))
 sdbound.bioclim <- spTransform(sdbound, crs(bioclim))
@@ -48,3 +48,14 @@ bioclim.crop <- crop(bioclim, sdbound.bioclim)
 
 
 dem.bioclim <- projectRaster(dem.crop, bioclim.crop)
+
+PredVars <- stack(bioclim.crop, dem.bioclim)
+
+
+Bison.sp.bioclim <- spTransform(Bison.sp, crs(PredVars))
+
+# Now we can plot everything and make sure they line up:
+plot(PredVars[[1]])  #Only plots the first layer from the stack of predictor variables
+plot(Bison.sp.bioclim, add = T)  #plot the locality points
+plot(sdbound.bioclim, add = T)  #plot the County boundary.
+
